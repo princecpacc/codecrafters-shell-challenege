314b8bee5b7b43c475fcd7ce59bb8b5ed77e8535
Prince <prince@Princes-MacBook-Air-3.local>
2026-06-17 12:09:47 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         System.out.print("$ ");
+        System.out.flush();
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        System.out.println(input + ": command not found");
     }
 }
