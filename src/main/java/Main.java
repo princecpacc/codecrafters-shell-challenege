270b31678997a703f92c1ef22944a6d040c518d3
Prince <prince@Princes-MacBook-Air-3.local>
2026-06-18 13:04:08 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,19 +16,21 @@ public class Main {
                 continue;
             }
 
-            // Split the input line by spaces to separate the command from arguments
             String[] commands = input.split(" ");
             String baseCommand = commands[0];
 
             if (baseCommand.equals("exit")) {
                 break;
             } else if (baseCommand.equals("echo")) {
-                // Slices off "echo " from the raw input line to print arguments properly
                 System.out.println(input.substring(5));
+            } else if (baseCommand.equals("pwd")) {
+                // Fetch and print the current working directory absolute path
+                System.out.println(System.getProperty("user.dir"));
             } else if (baseCommand.equals("type")) {
                 String commandToCheck = commands[1];
                 
-                if (commandToCheck.equals("echo") || commandToCheck.equals("exit") || commandToCheck.equals("type")) {
+                // Add "pwd" to the list of known shell builtins
+                if (commandToCheck.equals("echo") || commandToCheck.equals("exit") || commandToCheck.equals("type") || commandToCheck.equals("pwd")) {
                     System.out.println(commandToCheck + " is a shell builtin");
                 } else {
                     String path = getPath(commandToCheck);
@@ -39,21 +41,18 @@ public class Main {
                     }
                 }
             } else {
-                // If it's not a builtin, check if it exists in the PATH environment variable
                 String executablePath = getPath(baseCommand);
                 
                 if (executablePath != null) {
-                    // Collect the base command and arguments to pass to ProcessBuilder
                     List<String> commandList = new ArrayList<>();
                     for (String arg : commands) {
                         commandList.add(arg);
                     }
 
-                    // Create and start the external OS process
                     ProcessBuilder pb = new ProcessBuilder(commandList);
-                    pb.inheritIO(); // Connects the process streams to the current terminal
+                    pb.inheritIO(); 
                     Process process = pb.start();
-                    process.waitFor(); // Block until execution finishes
+                    process.waitFor(); 
                 } else {
                     System.out.println(input + ": not found");
                 }
@@ -61,7 +60,6 @@ public class Main {
         }
     }
 
-    // Helper method to scan the PATH environment variable for an executable
     private static String getPath(String command) {
         String pathEnv = System.getenv("PATH");
         if (pathEnv != null) {
